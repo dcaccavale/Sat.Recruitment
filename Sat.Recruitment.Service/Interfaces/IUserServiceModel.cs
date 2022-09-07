1926975de5469237c1781bd55184acf1ab0a24a1
Dario <dario.caccavale@kognitiv.com>
2022-09-06 21:34:12 -0300

    Refactoring service model layer

--- Sat.Recruitment.Service/Interfaces/IUserServiceModel.cs
+++ Sat.Recruitment.Service/Interfaces/IUserServiceModel.cs
@@ -1,6 +1,11 @@
-﻿namespace Sat.Recruitment.Service
+﻿using Sat.Recruitment.Model.Request;
+using Sat.Recruitment.Model.Response;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.Service
 {
     public interface IUserServiceModel
     {
+        Task<UserResponse> Create(UserRequest userRequest);
     }
 }
--- Sat.Recruitment.Service/UserServiceModel.cs
+++ Sat.Recruitment.Service/UserServiceModel.cs
@@ -5,7 +5,7 @@ using Sat.Recruitment.Model.Calculate;
 using Sat.Recruitment.Model.Entities;
 using Sat.Recruitment.Model.Request;
 using Sat.Recruitment.Model.Response;
-using Sat.Recruitment.Service.Validations;
+using Sat.Recruitment.Model.Request.Validations;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -17,24 +17,30 @@ namespace Sat.Recruitment.Service
     {
         public IUserRepository _userRepository;
         public IMapper _mapper;
-        public ILogger _logger;
-        public UserServiceModel(IUserRepository userRepository, IMapper mapper, ILogger logger)
+        public ILogger<User> _logger;
+        public UserServiceModel(IUserRepository userRepository, IMapper mapper, ILogger<User> logger)
         {
             _userRepository = userRepository;
             _mapper = mapper;
             _logger = logger;
         }
 
+        /// <summary>
+        /// Create and add a new User, check for duplicates and required fields
+        /// </summary>
+        /// <param name="userRequest"></param>
+        /// <returns></returns>
+        /// <exception cref="ValidationException">invalid required fields or user is duplicated</exception>
         public async Task<UserResponse> Create(UserRequest userRequest)
         {
-       
+
             User aUser;
             UserRequestValidate userValidate = new UserRequestValidate();
-            var validatorResult =  userValidate.Validate(userRequest);
+            var validatorResult = userValidate.Validate(userRequest);
             if (validatorResult.IsValid)
             {
-               
-                if (! await IsDuplicated(userRequest, _userRepository))
+
+                if (!await IsDuplicated(userRequest, _userRepository))
                 {
                     aUser = _mapper.Map<User>(userRequest);
                     aUser.Money = CalculateUserAmountManager.GetCalculate(aUser.Type).Execute(aUser.Money);
@@ -52,15 +58,15 @@ namespace Sat.Recruitment.Service
             ///Not valid argument
             {
                 _logger.Log(LogLevel.Error, "Invalid userRequest field", validatorResult.ToString(" - "));
-                throw new ValidationException (validatorResult.ToString(" - "));
+                throw new ValidationException(validatorResult.ToString(" - "));
             }
-             
+
         }
 
-        private  Task<bool> IsDuplicated(UserRequest userRequest, IUserRepository userRepository)
+        private Task<bool> IsDuplicated(UserRequest userRequest, IUserRepository userRepository)
         {
-            return  _userRepository.Any<User>(user => (user.Email == userRequest.Email || user.Phone == userRequest.Phone)
-            || (user.Name == userRequest.Name &&  user.Address == userRequest.Address)
+            return _userRepository.Any<User>(user => (user.Email == userRequest.Email || user.Phone == userRequest.Phone)
+            || (user.Name == userRequest.Name && user.Address == userRequest.Address)
             );
         }
     }
