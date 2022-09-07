1d0fd435a2a10fd2ddefa99d0d167de3bc6b3b0c
Dario <dario.caccavale@kognitiv.com>
2022-09-06 23:54:39 -0300

    Add  DataAccess Layer unit Test

--- Sat.Recruitment.Test/DataAccessLayerUnitTest.cs
+++ Sat.Recruitment.Test/DataAccessLayerUnitTest.cs
@@ -1,19 +1,23 @@
 using System;
 using System.Collections.Generic;
-
+using System.Data.Entity.Infrastructure;
 using System.Dynamic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
-
+using Sat.Recruitment.Api;
 using Sat.Recruitment.DataAccess;
 using Sat.Recruitment.DataAccess.Repositories;
 using Sat.Recruitment.Model.Entities;
+using Sat.Recruitment.Test.Helpers;
 using Xunit;
 
 namespace Sat.Recruitment.Test
@@ -21,59 +25,172 @@ namespace Sat.Recruitment.Test
     [CollectionDefinition("DataAccessLayerUnitTest", DisableParallelization = true)]
     public class DataAccessLayerUnitTest
     {
-        private UserRepository _UserRepository;
-        private Mock<SatRecruitmentContext> _mockSatContext;
 
+        private readonly IUserRepository _UserRepository;
         public DataAccessLayerUnitTest()
         {
-            var data = new List<User>()
-            {
-                new User()
-                {
-                    IdGuid = Guid.NewGuid(),
-                    Id = 1,
-                    Email ="dos@doc.com",
-                    Phone="22222222",
-                    Name = "Name 1"
-                },
-                 new User()
-                {
-                    IdGuid = Guid.NewGuid(),
-                    Id = 1,
-                    Email ="dos@doc.com",
-                    Phone="22222222",
-                    Name = "Name 1"
-                }
+            var webHost = WebHost.CreateDefaultBuilder()
+              .UseStartup<Startup>()
+              .Build();
+            _UserRepository = new DependencyResolverHelper(webHost).GetService<IUserRepository>();
 
-        }.AsQueryable();
+        }
 
-            var mockSet = new Mock<DbSet<User>>();
-            mockSet.As<IQueryable<User>>().Setup(m => m.Provider).Returns(data.Provider);
-            mockSet.As<IQueryable<User>>().Setup(m => m.Expression).Returns(data.Expression);
-            mockSet.As<IQueryable<User>>().Setup(m => m.ElementType).Returns(data.ElementType);
-            mockSet.As<IQueryable<User>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-            var optionsBuilder = new DbContextOptions<SatRecruitmentContext>();
-            _mockSatContext = new Mock<SatRecruitmentContext>(optionsBuilder);
-            Mock<DatabaseFacade> databaseFacade = new Mock<DatabaseFacade>(_mockSatContext.Object);
-            databaseFacade.Setup(d => d.EnsureCreatedAsync(CancellationToken.None)).Returns(Task.FromResult(true));
-            _mockSatContext.Setup(c => c.Database).Returns(databaseFacade.Object);
-            _mockSatContext.Setup(c => c.Users).Returns(mockSet.Object);
+        /// <summary>
+        /// Test Get All Async
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task FirstOrDefaultAsync()
+        {
+
+            var result = await _UserRepository.FirstOrDefaultAsync<User>(u=> u.Id == 1);
+           // var okResult = result as ObjectResult;
+            Assert.NotNull(result);
+            Assert.True(1 == result.Id);
+        }
+
+        /// <summary>
+        /// Test Get All Async
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task GetAllAsync()
+        {
            
-            _UserRepository = new UserRepository(_mockSatContext.Object);
+            var result = await _UserRepository.GetAllAsync<User>();
+            Assert.NotNull(result);
+            Assert.True( result.Count() > 0);
+        }
+
+        /// <summary>
+        /// Test Get All Async
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task GetAllAsyncByCriteria()
+        {
+
+            var result = await _UserRepository.GetAllAsync<User>(u=> u.Id == 1);
+            Assert.NotNull(result);
+            Assert.True(result.Count() == 1 && result[0].Id == 1);
+        }
+
+        /// <summary>
+        /// Test GetAsync
+        /// </summary>
+        /// <returns></returns>
+        [Fact]
+        public async Task GetAsync()
+        {
+
+            var Auxresult = await _UserRepository.GetAllAsync<User>(u => u.Id == 1);
+            var result = await _UserRepository.GetAsync<User>(Auxresult[0].IdGuid);
+            Assert.NotNull(result);
+            Assert.True(Auxresult[0].IdGuid == result.IdGuid);
+        }
+
+
+        [Fact]
+        public async Task Any()
+        {
+            Assert.True(await _UserRepository.Any<User>(u => u.Id == 1));
+        }
+
+
+        [Fact]
+        public async Task Add()
+        {
+            User user = new User
+            {
+                IdGuid = Guid.NewGuid(),
+                Name = "prueba" + Guid.NewGuid().ToString(),
+                Email = Guid.NewGuid() + "prueba@gmail.com",
+                Address = Guid.NewGuid() + "calle charles 855",
+                Phone = Guid.NewGuid() + "5255555555",
+                Type = UserType.Normal,
+                Money = 125
+            };
+            var result = await _UserRepository.Add<User>(user);
+            var userAdd = await _UserRepository.FirstOrDefaultAsync<User>(u=> u.Id == user.Id);
+            Assert.NotNull(userAdd);
+            Assert.True(userAdd.IdGuid == user.IdGuid && userAdd.Id == user.Id && userAdd.Address == user.Address &&
+                userAdd.Email == user.Email && userAdd.Name == user.Name && userAdd.Phone == user.Phone && userAdd.Type == user.Type);
+            Assert.True(user.State == StateEntity.Created);
         }
 
         [Fact]
-        public void TEstRepo()
+        public async Task Update()
         {
+            User user = new User
+            {
+                IdGuid = Guid.NewGuid(),
+                Name = "prueba" + Guid.NewGuid().ToString(),
+                Email = Guid.NewGuid() + "prueba@gmail.com",
+                Address = Guid.NewGuid() + "calle charles 855",
+                Phone = Guid.NewGuid() + "5255555555",
+                Type = UserType.Normal,
+                Money = 125
 
-            var value = _UserRepository.GetAllAsync<User>().Result;
-            Assert.Null(value);
+            };
+            var result = await _UserRepository.Add<User>(user);
+            var userAdd = await _UserRepository.FirstOrDefaultAsync<User>(u => u.Id == user.Id);
+            userAdd.Name = userAdd.Name + "Modificate";
+            var userUpdate = await _UserRepository.Update<User>(userAdd);
+
+            Assert.NotNull(userUpdate);
+            Assert.True(userUpdate.State == StateEntity.Updated);
+
         }
+
 
         [Fact]
-        public void Test2()
+        public async Task Delete()
         {
+            User user = new User
+            {
+                IdGuid = Guid.NewGuid(),
+                Name = "prueba" + Guid.NewGuid().ToString(),
+                Email = Guid.NewGuid() + "prueba@gmail.com",
+                Address = Guid.NewGuid() + "calle charles 855",
+                Phone = Guid.NewGuid() + "5255555555",
+                Type = UserType.Normal,
+                Money = 125
+
+            };
+            var result = await _UserRepository.Add<User>(user);
+            var userAdd = await _UserRepository.FirstOrDefaultAsync<User>(u => u.Id == user.Id);
+           
+            var userDelete = await _UserRepository.Delete<User>(userAdd);
+
+            Assert.NotNull(userDelete);
+            Assert.True(userDelete.State == StateEntity.Deleted);
 
         }
+
+
+        [Fact]
+        public async Task Remove()
+        {
+            User user = new User
+            {
+                IdGuid = Guid.NewGuid(),
+                Name = "prueba" + Guid.NewGuid().ToString(),
+                Email = Guid.NewGuid() + "prueba@gmail.com",
+                Address = Guid.NewGuid() + "calle charles 855",
+                Phone = Guid.NewGuid() + "5255555555",
+                Type = UserType.Normal,
+                Money = 125
+
+            };
+            var result = await _UserRepository.Add<User>(user);
+            var userAdd = await _UserRepository.FirstOrDefaultAsync<User>(u => u.Id == user.Id);
+            await _UserRepository.Remove<User>(userAdd);
+            var userRemove = await _UserRepository.FirstOrDefaultAsync<User>(u => u.Id == user.Id);
+            Assert.Null(userRemove);
+        
+        }
+  
+
     }
 }
