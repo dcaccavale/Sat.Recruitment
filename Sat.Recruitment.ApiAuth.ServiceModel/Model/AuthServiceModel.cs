264ad5fcf2b8aec558e35fa2c9a41106d494dc2e
Dario <dario.caccavale@kognitiv.com>
2022-10-15 00:06:40 -0300

    Nothing

---
+++ Sat.Recruitment.ApiAuth.ServiceModel/Model/AuthServiceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sat.Recruitment.ApiAuth.ServiceModel.Model
+{
+    internal class AuthServiceModel
+    {
+    }
+}
